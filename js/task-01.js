e537503e1d2c61ba107a102056ce8f822d4d82d0
Rudakova1007 <110676149+Rudakova1007@users.noreply.github.com>
2023-01-09 15:48:41 +0200

    bug fixing
    bug fixing

--- js/task-01.js
+++ js/task-01.js
@@ -7,10 +7,12 @@ displayCategoryContent(categoryArray);
 function displayCategoryContent(catArray){
     let category = null;
     for(let i = 0; i < catArray.length; i += 1){
-        console.log("Category: " + catArray[i].children[0].textContent);
+        // console.log("Category: " + catArray[i].children[0].textContent);
+        console.log("Category: " + list2[i].firstElementChild.textContent);
 
         let categoryItems = catArray[i].children[1].children
-        console.log("Elements: " + categoryItems.length)
+        // console.log("Elements: " + categoryItems.length)
+        console.log("Elements: " + list2[i].lastElementChild.childElementCount);
     }
 }
 
--- js/task-02.js
+++ js/task-02.js
@@ -1,17 +1,33 @@
+// const ingredients = [
+//   'Potatoes',
+//   'Mushrooms',
+//   'Garlic',
+//   'Tomatos',
+//   'Herbs',
+//   'Condiments',
+// ];
+
+// const ingredientsListElement = document.getElementById("ingredients");
+// for(let i = 0; i < ingredients.length; i += 1){
+//   let itemElement = document.createElement("li");
+//   itemElement.classList.add("item");
+//   itemElement.textContent = ingredients[i];
+//   ingredientsListElement.append(itemElement);
+// }
+
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
-
-const ingredientsListElement = document.getElementById("ingredients");
-for(let i = 0; i < ingredients.length; i += 1){
-  let itemElement = document.createElement("li");
-  itemElement.classList.add("item");
-  itemElement.textContent = ingredients[i];
-  ingredientsListElement.append(itemElement);
-}
-
+const ingredientsList = document.getElementById("ingredients");
+ingredientsList.append(
+  ...ingredients.map((item) => {
+    let itemList = document.createElement("li");
+    itemList.textContent = item;
+    return itemList;
+  })
+);
